3a7fc3512632fc0be727a61ef9b48ab0a8b31341
WildTooth <champenV@outlook.dk>
2023-02-01 02:17:53 +0100

    Added more to StringUtils.java

--- src/main/java/org/supernederen/awesomelib/utils/StringUtils.java
+++ src/main/java/org/supernederen/awesomelib/utils/StringUtils.java
@@ -1,5 +1,9 @@
 package org.supernederen.awesomelib.utils;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
 public class StringUtils {
 
     private StringUtils() {
@@ -180,5 +184,41 @@ public class StringUtils {
      */
     public static boolean isNothing(String str) {
         return str != null && !str.isEmpty();
+    }
+
+    /**
+     * Returns a formatted String from a List of Strings.
+     * if the list is empty, it will return a predetermined string.
+     *
+     * @param strings list of strings to format
+     * @param placeholder the placeholder to replace
+     *                    if the list is not empty
+     * @return formatted string
+     */
+    public static String formatList(@NotNull List<String> strings, String placeholder){
+        if(strings.isEmpty()) return placeholder;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strings.size(); i++) {
+            if (i != 0)
+                sb.append(i == strings.size() - 1 ? " " + "og" + " " : ", ");
+            sb.append(strings.get(i));
+        }
+        return sb.toString();
+    }
+
+    /**
+     *
+     * Returns a boolean value from a string.
+     *
+     * @param paramString the string to check
+     * @param paramStrings the strings to check against
+     *
+     * @return if the string ends with any of the strings
+     */
+    public static boolean endsWith(String paramString, String @NotNull ... paramStrings){
+        for(String s : paramStrings)
+            if(paramString.substring(paramString.length()-s.length()).equalsIgnoreCase(s))
+                return true;
+        return false;
     }
 }

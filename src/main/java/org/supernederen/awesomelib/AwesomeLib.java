c5bb61d1ad7be649652ad6dafd6076061c142c98
WildTooth <champenV@outlook.dk>
2023-02-01 01:14:14 +0100

    #getInstance() method

--- src/main/java/org/supernederen/awesomelib/AwesomeLib.java
+++ src/main/java/org/supernederen/awesomelib/AwesomeLib.java
@@ -1,11 +1,16 @@
 package org.supernederen.awesomelib;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AwesomeLib extends JavaPlugin {
 
+    @Getter
+    private static AwesomeLib instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         // Plugin startup logic
 
     }
